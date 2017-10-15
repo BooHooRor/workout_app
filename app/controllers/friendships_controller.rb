d9e4a18ee32670e1722dc57d4a8553755e322c47
Kris <buchowiecki.krzysztof@gmail.com>
2017-10-15 15:39:44 +0200

    Create chatroom when user signs up

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -11,6 +11,7 @@ class FriendshipsController < ApplicationController
 		params[:user_id] = current_user.id
 
 		Friendship.create(friendship_params) unless current_user.follows_or_same?(friend)
+		
 		redirect_to root_path
 	end
 
